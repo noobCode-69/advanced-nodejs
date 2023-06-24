febb85dfc94e114597e8a4354d7bcadb86028d34
so_hell <armanyppph@gmail.com>
2023-06-24 15:27:03 +0530

    final commit

--- index.js
+++ index.js
@@ -1,33 +1,22 @@
-process.env.UV_THREADPOOL_SIZE = 1;
-const cluster = require("cluster");
 const crypto = require("crypto");
+const express = require("express");
+const PORT = 3000;
 
-// Is the file begin executed in master mode?
-if (cluster.isMaster) {
-  // Cause index.js to be executed *again* but
-  // in slave mode
-  cluster.fork();
-  cluster.fork();
-} else {
-  // I'm a child , Im going to act like a server
+const app = express();
 
-  const express = require("express");
-  const PORT = 3000;
-  const app = express();
-
-  app.get("/", (req, res) => {
-    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
-      res.send("Hi there!");
-    });
+app.get("/", (req, res) => {
+  crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+    res.send("Hi there!");
   });
+});
 
-  app.get("/fast", (req, res) => {
-    res.send("This was fast");
-  });
+app.get("/fast", (req, res) => {
+  res.send("This was fast");
+});
 
-  app.listen(PORT, () => {
-    console.log(`Server started at port ${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server started at port ${PORT}`);
+});
 
-// Number of child process must be not more that the number of physical core
+// pm2 start index.js -i 0
+// it will set the number of child process to number of logical cores
